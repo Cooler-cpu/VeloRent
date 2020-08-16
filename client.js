b2cf6dac430a7f93d8ec55975b3c1a28d0f93f29
Vladislav Babichev <vpolozhilov@bk.ru>
2020-08-17 02:36:31 +0300

    test

--- client.js
+++ client.js
@@ -1,41 +1,41 @@
-// let header=document.getElementsByClassName("header")[0],
+let header=document.getElementsByClassName("header")[0],
 
-// tmp = ['b3.png', 'b2.png', 'b1.png'],
-// i=0;
+tmp = ['b3.png', 'b2.png', 'b1.png'],
+i=0;
 
-// setInterval(function(){
+setInterval(function(){
     
-//     //console.log(header.classList);
-//     header.classList.remove('anim');
-//     header.classList.add('anim2');
+    //console.log(header.classList);
+    header.classList.remove('anim');
+    header.classList.add('anim2');
 
-//    // header.classList.remove('anim');
-//     setTimeout(() => {
+   // header.classList.remove('anim');
+    setTimeout(() => {
       
-//         header.classList.add('anim');
-//     if (i==tmp.length)
-//         i=0;
-//     //console.log(i);
-//     header.style.background = 'url("img/'+tmp[i]+'")';
-//     header.style.backgroundRepeat = 'no-repeat';
-//     header.style.backgroundSize = 'cover';
-//     i++;
-// }, 3000);
+        header.classList.add('anim');
+    if (i==tmp.length)
+        i=0;
+    //console.log(i);
+    header.style.background = 'url("img/'+tmp[i]+'")';
+    header.style.backgroundRepeat = 'no-repeat';
+    header.style.backgroundSize = 'cover';
+    i++;
+}, 3000);
 
-//     header.classList.remove('anim2');
+    header.classList.remove('anim2');
     
 
-// },10000)
+},10000)
 
-// //Получаем id modal
+//Получаем id modal
 
-// let modal = document.getElementById("modal_content");
+let modal = document.getElementById("modal_content");
 
-// open_modal_window = () => {
-//     modal.style.display = "block"
-//     console.log("1");
-// }
+open_modal_window = () => {
+    modal.style.display = "block"
+    console.log("1");
+}
 
-// close_modal_window = () => {
+close_modal_window = () => {
 
-// }
+}
