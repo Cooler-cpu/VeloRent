83f4876c245956a204a66d3ed9aa47e471e4b41a
Vladislav Babichev <vpolozhilov@bk.ru>
2020-08-19 01:11:14 +0300

    adaptive animation

--- client.js
+++ client.js
@@ -1,41 +1,61 @@
-// let header=document.getElementsByClassName("header")[0],
+let header=document.getElementsByClassName("header")[0],
 
-// tmp = ['b3.png', 'b2.png', 'b1.png'],
-// i=0;
+tmp = ['b3.png', 'b2.png', 'b1.png'],
+i=0;
 
-// setInterval(function(){
+console.log("ширина экрана:", window.innerWidth);
+
+
+setInterval(function(){
+    if(window.innerWidth <= 1080){
+        header.style.background = 'url("img/'+"bg_adapt.png"+'")';
+        header.style.backgroundRepeat = 'no-repeat';
+        header.style.backgroundSize = 'cover';
+    }
+
+
+},2000);
+
+setInterval(function(){
     
-//     //console.log(header.classList);
-//     header.classList.remove('anim');
-//     header.classList.add('anim2');
+        
+    if(window.innerWidth > 1080){
 
-//    // header.classList.remove('anim');
-//     setTimeout(() => {
-      
-//         header.classList.add('anim');
-//     if (i==tmp.length)
-//         i=0;
-//     //console.log(i);
-//     header.style.background = 'url("img/'+tmp[i]+'")';
-//     header.style.backgroundRepeat = 'no-repeat';
-//     header.style.backgroundSize = 'cover';
-//     i++;
-// }, 3000);
+                //console.log(header.classList);
+                header.classList.remove('anim');
+                header.classList.add('anim2');
 
-//     header.classList.remove('anim2');
+            // header.classList.remove('anim');
+                setTimeout(() => {
+                
+                    header.classList.add('anim');
+                if (i==tmp.length)
+                    i=0;
+                //console.log(i);
+                header.style.background = 'url("img/'+tmp[i]+'")';
+                header.style.backgroundRepeat = 'no-repeat';
+                header.style.backgroundSize = 'cover';
+                i++;
+            }, 3000);
+
+            header.classList.remove('anim2');
     
+        }
 
-// },10000)
+},10000)
 
-// //Получаем id modal
 
-// let modal = document.getElementById("modal_content");
 
-// open_modal_window = () => {
-//     modal.style.display = "block"
-//     console.log("1");
-// }
+//Получаем id modal
 
-// close_modal_window = () => {
+let modal = document.getElementById("modal_content");
 
-// }
+
+open_modal_window = () => {
+    modal.style.display = "block"
+    console.log("1");
+}
+
+close_modal_window = () => {
+
+}
